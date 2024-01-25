da7f3737f987d53580d8c0c2fc3aa65b93e9adb4
Netulo <matteu06@gmail.com>
2024-01-25 11:21:37 +0100

    2023 - day 8 in progress

--- 2023/Day 8/8-p2.cpp
+++ 2023/Day 8/8-p2.cpp
@@ -2,14 +2,16 @@
 #include<string>
 #include<fstream>
 #include<map>
+#include<vector>
+#include <algorithm>
 
 using namespace std;
 
-int countStep(string mapID, string instruction, map<string, string> maps, int count);
+int countStep(string mapID, string instruction, map<string, string> maps, map<string, int> &startingPoints, bool isFirst);
 
 int main()
 {
-    fstream myFile("test.txt");
+    fstream myFile("test-p2.txt");
 
     if(!myFile.is_open())
     {
@@ -36,26 +38,55 @@ int main()
         
     }
 
-    // for(auto x : maps)
+    for(auto x : maps)
+        cout << x.first << " -> " << x.second << endl;
+
+    map<string, int> startingPoints;
+    for(auto x : maps)
+        if(x.first[2] == 'A')
+            startingPoints[x.first] = 0;
+
+    // for(auto x : startingPoints)
     //     cout << x.first << " -> " << x.second << endl;
 
-    printf("Step count is: %d", countStep("aaa", instruction, maps, 0));
+    int count;
+    while(true)
+    {
+        for(auto x : startingPoints)
+        {
+            countStep(x.first, instruction, maps, startingPoints, true);
+            count = startingPoints[x.first];
+        }
+
+        for(auto x : startingPoints)
+            cout << x.first << " -> " << x.second << endl;
+        
+        cout << endl;
+
+        if(all_of(startingPoints.begin(), startingPoints.end(), [&](auto i){return i.second == startingPoints.begin() -> second;}))
+            break;
+    }
+
+
+    printf("Step count is: %d", count);
 }
 
 
 
-int countStep(string mapID, string instruction, map<string, string> maps, int count)
+int countStep(string mapID, string instruction, map<string, string> maps, map<string, int> &startingPoints, bool isFirst)
 {
 
-    if(mapID == "ZZZ")
+    if(mapID.substr(2, 1) == "Z" && !isFirst)
         return 0;
 
-        if(instruction[count%instruction.length()] == 'L')
-            mapID = maps[mapID].substr(1, 3);
-        else
-             mapID = maps[mapID].substr(6, 3);
+    int count = startingPoints[mapID];
+    startingPoints.erase(mapID);
+    if(instruction[count%instruction.length()] == 'L')
+        mapID = maps[mapID].substr(1, 3);
+    else
+        mapID = maps[mapID].substr(6, 3);
 
-        count++;
+    startingPoints[mapID] = count+1;
 
-    return countStep(mapID, instruction, maps, count) + 1;
+    return countStep(mapID, instruction, maps, startingPoints, false) + 1;
 }
