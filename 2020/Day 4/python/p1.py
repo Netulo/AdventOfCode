844ec9d590f47c20e3a5f1e1e36aa16865117df2
Netulo <matteu06@gmail.com>
2024-03-08 08:37:22 +0100

    2020 Day 4 p1 ptimalised

--- 2020/Day 4/python/p1.py
+++ 2020/Day 4/python/p1.py
@@ -1,26 +1,5 @@
-f = open("2020/Day 4/input.txt", "r")
-
-count = 0
-
-fields = ["byr","iyr","eyr","hgt","hcl" ,"ecl","pid"]
-fields.sort()
-table = []
-for x in f.read().split("\n\n"):
-    dic = dict()
-    for y in x.replace('\n', ' ').split(' '):
-        temp = y.split(':')
-        dic[temp[0]] = temp[1]
-        
-    table.append(dic)
-    
-for x in table:
-    tab = list(x.keys())
-    tab.sort()
-    try:
-        tab.remove("cid")
-    except:
-        pass
-    if tab == fields:
-        count+=1
-        
-print(count)
+sum = 0
+for x in open("2020/Day 4/input.txt", "r").read().split("\n\n"):
+    if all(y in x for y in ["byr","iyr","eyr","hgt","hcl" ,"ecl","pid"]):
+        sum += 1
+print(sum)
