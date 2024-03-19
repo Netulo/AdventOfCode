be3c5ffc991d307be66aa0fe58dff69879bf3cc6
Netulo <matteu06@gmail.com>
2024-03-19 12:07:49 +0100

    Working on day 4 2021

--- 2021/Day 4/python/p1-v1.py
+++ 2021/Day 4/python/p1-v1.py
@@ -1,10 +1,3 @@
-import re
-
-f = open('2021/Day 4/input.txt', 'r')
-numberSet = f.readline().split(',')
-f.readline()
-boards = f.read().split('\n\n')
-
 def SumOfTable(table):
     sum = 0
     for x in table:
@@ -16,22 +9,29 @@ def SumOfTable(table):
     return sum
 
 def checkIfBingo(numberSet, boards):
-    for x in numberSet:
-        for y in range(len(boards)):
-            for z in range(len(boards[y])):
-                if x in boards[y][z]:
+    for num in numberSet:
+        for board in range(len(boards)):
+            for row in range(len(boards[board])):
+                if num in boards[board][row]:
                     while 1:
                         try:
-                            answ = boards[y][z].index(x)
-                            boards[y][z][answ] = '*' + boards[y][z][answ]
+                            answ = boards[board][row].index(num)
+                            boards[board][row][answ] = '*' + boards[board][row][answ]
                         except:
                             break
                 
-                if all('*' in i for i in boards[y][z]):
-                    boards[y].pop(z)
-                    return int(x) * SumOfTable(boards[y])
+                if all('*' in i for i in boards[board][row]):
+                    boards[board].pop(row)
+                    return int(num) * SumOfTable(boards[board])
          #sprawdzanie kolumn - brak
+            if all(e)
     return 0
+
+
+f = open('2021/Day 4/input.txt', 'r')
+numberSet = f.readline().split(',')
+f.readline()
+boards = f.read().split('\n\n')
 
 for x in range(len(boards)):
     temp = boards[x].split('\n')
