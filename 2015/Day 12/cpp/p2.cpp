47a220d87a8dfa1415101ae0bd816de0eb1e6444
Mateusz Kubiak <matteu06@gmail.com>
2024-11-29 08:39:18 +0100

    Some changes

--- 2015/Day 12/cpp/p2.cpp
+++ 2015/Day 12/cpp/p2.cpp
@@ -11,6 +11,41 @@ int main(){
     string inp;
     getline(f, inp);
 
+    int temp = 0;
+    int i = 0;
+    while(true){
+        if(i >= inp.length()){
+            if(temp == 0)
+                break;
+            temp = 0;
+            i = 0;
+            continue;
+        }
+
+        cout << temp;
+
+        int leftClose = inp.find("{", i);
+        int rightClose = inp.find("}", i);
+        if(leftClose < rightClose){
+            i = leftClose;
+            continue;
+        }
+        else if(leftClose > rightClose){
+            if(inp.find("red", i, rightClose-i+1) != string::npos){
+                inp.erase(i, rightClose-i+1);
+                temp++;
+                cout << temp;
+            }
+        }
+
+        i++;
+    }
+
+
+
+
+
+
     int sum = 0;
     for(int i = 0; i < inp.length(); i++){
         if((int(inp[i]) >= (int)'0' && (int)inp[i]<=(int)'9') || inp[i] == '-'){
